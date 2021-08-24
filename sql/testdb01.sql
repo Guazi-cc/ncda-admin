48a8dac8d043e6811e60a7d6903ebd1c4ec56692
chenda <chenda.me@foxmail.com>
2021-08-24 16:45:11 +0800

    更新sql文件

--- sql/testdb01.sql
+++ sql/testdb01.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50734
  File Encoding         : 65001
 
- Date: 20/08/2021 17:41:29
+ Date: 24/08/2021 16:43:52
 */
 
 SET NAMES utf8mb4;
@@ -32,7 +32,7 @@ CREATE TABLE `account_bill`  (
   `ITEM_NAME` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '名称',
   `USER_ID` int(11) NULL DEFAULT NULL COMMENT '用户ID',
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 166 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 190 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for account_bill_type
@@ -58,7 +58,7 @@ CREATE TABLE `account_bill_upload_record`  (
   `DEL_STATE` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '删除状态（0删除，1正常）',
   `UPLOAD_TIME` datetime(0) NULL DEFAULT NULL COMMENT '上传时间',
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ncda_user
@@ -67,6 +67,9 @@ DROP TABLE IF EXISTS `ncda_user`;
 CREATE TABLE `ncda_user`  (
   `USER_ID` int(11) NOT NULL,
   `USER_NAME` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `GENDER` int(11) NULL DEFAULT NULL,
+  `BIRTHDAT` date NULL DEFAULT NULL,
+  `REAL_NAME` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`USER_ID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
